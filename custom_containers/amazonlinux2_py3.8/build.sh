9150dea989ae13e9bfba207e8be7284fe23336fa
Colton Hicks <coltonbhicks@gmail.com>
2020-03-20 12:45:06 -0700

    inital commit

---
+++ custom_containers/amazonlinux2_py3.8/build.sh
@@ -0,0 +1,3 @@
+docker build . -t coltonbh/amazonlinux2-py3.8:2.0.20200207.1_py3.8.2 -t coltonbh/amazonlinux2-py3.8:2_py3.8
+docker push coltonbh/amazonlinux2-py3.8:2.0.20200207.1_py3.8.2
+docker push coltonbh/amazonlinux2-py3.8:2_py3.8
